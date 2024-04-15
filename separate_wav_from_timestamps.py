398ae5fa02da86f175ed7dd5caf9be97d6d1f65e
yusuke-hamanaka <yusuke.hamanaka@sony.com>
2024-04-15 15:36:48 +0900

    modified according to the expected directory structure

--- separate_wav_from_timestamps.py
+++ separate_wav_from_timestamps.py
@@ -1,32 +1,35 @@
 from pydub import AudioSegment
 import numpy as np
 import pandas as pd
+import glob
+import os
 
 
-def split_wav_from_timestamps(file_name_wo_ext: str, ):
-    wav_file = f"recordings/{file_name_wo_ext}.WAV"
-    timestamps_file = f"timestamps/{file_name_wo_ext}.txt"
+def split_wav_from_timestamps(folder_name: str = 'recordings') -> None:
+    # folder_name配下にあるすべてのサブフォルダを取得
+    subfolders = [os.path.join(folder_name, subfolder) for subfolder in os.listdir(folder_name)
+                  if os.path.isdir(os.path.join(folder_name, subfolder))]
+    
+    for subfolder in subfolders:
+        wav_file_name = [f for f in os.listdir(subfolder) if f.endswith(('.wav', '.WAV'))][0]
+        wav_file = os.path.join(subfolder, wav_file_name)
+        timestamps_file = os.path.join(subfolder, f"{wav_file_name.split('.')[0]}.txt")
 
-    channels = load_and_split_channels(wav_file)
-    selected_channel = get_louder_and_unsaturated_channel(channels)
-    timestamps_df = pd.read_csv(timestamps_file, sep='\t', header=None, names=["start", "end", "sound_type"])
-
-    for _, row in timestamps_df.iterrows():
-        start_time = int(row['start'] * 1000)  # milliseconds
-        end_time = int(row['end'] * 1000)      # milliseconds
+        # オーディオファイルを読み込み、チャンネルを分割した後、サチっていないかつ音量の大きいチャンネルを選択
+        audio = AudioSegment.from_wav(wav_file)
+        channels = audio.split_to_mono()
+        selected_channel = get_louder_and_unsaturated_channel(channels)
         
         # タイムスタンプに基づいてオーディオを分割
-        segment = selected_channel[start_time:end_time]
-        
-        # 分割されたセグメントをファイルに保存
-        output_file_name = f"{row['sound_type']}_{int(row['start'])}_{int(row['end'])}.wav"
-        segment.export(output_file_name, format="wav")
+        timestamps_df = pd.read_csv(timestamps_file, sep='\t', header=None, names=["start", "end", "sound_type"])
+        for _, row in timestamps_df.iterrows():
+            start_time = int(row['start'] * 1000)
+            end_time = int(row['end'] * 1000)
+            segment = selected_channel[start_time:end_time]
 
-def load_and_split_channels(wav_file):
-    audio = AudioSegment.from_wav(wav_file)
-    # ステレオトラックの場合、チャンネルを分割
-    channels = audio.split_to_mono()
-    return channels
+            output_file_name = f"{os.path.basename(subfolder)}_1_{row['sound_type']}.wav"
+            segment.export(output_file_name, format="wav")
+
 
 def get_louder_and_unsaturated_channel(channels):
     # RMSレベルに基づいて、より音量の大きいチャンネルを返す
@@ -46,5 +49,5 @@ def get_louder_and_unsaturated_channel(channels):
     return channels[selected_channel_index]
 
 
-split_wav_from_timestamps("240219_05")
-
+if __name__ == '__main__':
+    split_wav_from_timestamps()
