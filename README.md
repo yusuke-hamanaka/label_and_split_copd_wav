d96ba2956cbfac16c0e8f56cd7ae63c0d18f79ec
yusuke-hamanaka <yusuke.hamanaka@sony.com>
2024-06-03 10:31:15 +0900

    make change to create exe

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-※recordings内のwavファイルはすべてダミーのファイル
+※ recordings内のwavファイルはすべてダミーのファイル
 
 # 手順
 1. `speech`内に、`{id}_(smartphone_)?.*.wav`の命名規則に該当するwavファイルを格納する
@@ -6,5 +6,5 @@
 3. `python get_label_timestamps.py`を実行し、`recordings`フォルダを選択する
 4. `SN001/ic_recorder, SN001/smartphone`内に、仮ラベルファイル`{id}_(smartphone_)?.*_tmp.txt`が作られる
 5. Audacityで仮ラベルファイルを修正し、`{id}_(smartphone_)?.*.txt`という名前で同じディレクトリに保存する
-6. `python separate_wav_from_timestamps.py`を実行する
+6. `separate_wav_from_timestamps.exe`を実行する
 7. `release`内に分割されたwavファイルが保存される
--- separate_wav_from_timestamps.py
+++ separate_wav_from_timestamps.py
@@ -1,4 +1,6 @@
 import os
+import tkinter as tk
+from tkinter import messagebox
 
 import numpy as np
 import pandas as pd
@@ -15,37 +17,30 @@ def split_wav_from_timestamps(folder_name: str = "recordings") -> None:
     for wav_file in wav_files_full_path:
         timestamps_file = f"{wav_file.split('.')[0]}.txt"
 
-        # オーディオファイルを読み込み、チャンネルを分割した後、サチっていないかつ音量の大きいチャンネルを選択
         audio = AudioSegment.from_wav(wav_file)
         channels = audio.split_to_mono()
         selected_channel = get_louder_and_unsaturated_channel(channels)
 
-        # タイムスタンプに基づいてオーディオを分割
-        timestamps_df = pd.read_csv(
-            timestamps_file, sep="\t", header=None, names=["start", "end", "sound_type"]
-        )
+        timestamps_df = pd.read_csv(timestamps_file, sep="\t", header=None, names=["start", "end", "sound_type"])
         for _, row in timestamps_df.iterrows():
             start_time = int(row["start"] * 1000)
             end_time = int(row["end"] * 1000)
             segment = selected_channel[start_time:end_time]
 
-            save_folder = "./release"
+            current_folder = os.getcwd()
+            save_folder = f"{current_folder}/release"
             machine_type = os.path.dirname(wav_file).split(os.sep)[-1]
             patient_id = os.path.basename(wav_file).split("_")[0]
-            output_file_name = (
-                f"{save_folder}/{machine_type}/{patient_id}_1_{row['sound_type']}.wav"
-            )
+            output_file_name = f"{save_folder}/{machine_type}/{patient_id}_1_{row['sound_type']}.wav"
+            os.makedirs(os.path.dirname(output_file_name), exist_ok=True)
             segment.export(output_file_name, format="wav")
 
 
 def get_louder_and_unsaturated_channel(channels: list[AudioSegment]) -> AudioSegment:
-    # RMSレベルに基づいて、より音量の大きいチャンネルを返す
     channels_array = [np.array(channel.get_array_of_samples()) for channel in channels]
     max_possible_value = np.iinfo(channels_array[0].dtype).max
     max_allowed_value = max_possible_value * 0.99
-    is_saturated = [
-        np.any(np.abs(channel) > max_allowed_value) for channel in channels_array
-    ]
+    is_saturated = [np.any(np.abs(channel) > max_allowed_value) for channel in channels_array]
     if all(is_saturated):
         raise ValueError("All channels are saturated")
     elif any(is_saturated):
@@ -58,5 +53,25 @@ def get_louder_and_unsaturated_channel(channels: list[AudioSegment]) -> AudioSeg
     return channels[selected_channel_index]
 
 
+def run_split_wav() -> None:
+    try:
+        split_wav_from_timestamps()
+        messagebox.showinfo("Success", "Processing completed successfully.")
+    except Exception as e:
+        messagebox.showerror("Error", f"An error occurred: {str(e)}")
+
+
 if __name__ == "__main__":
-    split_wav_from_timestamps()
+    root = tk.Tk()
+    root.title("wav分割プログラム")
+
+    frame = tk.Frame(root, padx=20, pady=20)
+    frame.pack(padx=10, pady=10)
+
+    label = tk.Label(frame, text=f"recordingsフォルダ内の音声ファイルを分割します。")
+    label.pack(pady=10)
+
+    button = tk.Button(frame, text="実行", command=run_split_wav)
+    button.pack(pady=10)
+
+    root.mainloop()
